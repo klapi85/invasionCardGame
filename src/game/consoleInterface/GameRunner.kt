22376cfd48f3e0a4e76d57a38af90fd24edd9520
Wojciech Kuchta <wojciech.kuchta@allegro.pl>
2018-06-15 23:09:38 +0200

    GameRunner - commands with 1,2,3 parameters

--- src/game/consoleInterface/GameRunner.kt
+++ src/game/consoleInterface/GameRunner.kt
@@ -25,14 +25,14 @@ class GameRunner {
 
     private fun waitForInput(): String {
         val reader = BufferedReader(InputStreamReader(System.`in`))
-        println("Your move?")
+        println("Your move? (h for help)")
         return reader.readLine()
     }
 
     private fun waitForValidInput(): List<String> {
         val input = waitForInput()
         val cells = input.split(" ")
-        return if (isValidAction(cells[0]) and isValidNumber(cells[1])) cells else waitForValidInput()
+        return if (isValidAction(cells[0])) cells else waitForValidInput()
     }
 
     private fun isValidAction(input: String): Boolean = arrayOf("q", "u", "t", "i", "d", "p", "h", "l", "b").contains(input)
@@ -40,19 +40,40 @@ class GameRunner {
     private fun isValidNumber(input: String): Boolean = arrayOf("0", "1", "2").contains(input)
 
     private fun chooseMove(game: Game, options: List<String>) : Game {
+        return when (options.size) {
+            1 -> chooseOneParamMove(game, options[0])
+            2 -> chooseTwoParamsMove(game, options)
+            3 -> chooseThreeParamsMove(game, options)
+            else -> throw IllegalArgumentException("Actions: t,u,q,i,d,p,h space and number eg. t 2. For exit q 1")
+        }
+    }
+
+    private fun chooseOneParamMove(game: Game, option: String) : Game {
+        return when (option) {
+            "h" -> game.escapeGame(game) // TODO
+            "q" -> game.escapeGame(game)
+            else -> throw IllegalArgumentException("Wrong Actions. For exit q")
+        }
+    }
+
+    private fun chooseTwoParamsMove(game: Game, options: List<String>) : Game {
         return when (options[0]) {
             "u" -> makeUntapMove(game, options[1])
             "t" -> makeTapMove(game, options[1])
             "i" -> makeIncreasePowerMove(game, options[1])
             "d" -> makeIncreaseDefenceMove(game, options[1])
-            "p" -> playCardFromHand(game, options[1])
-            "l" -> playCardFromHandToKingdom(game, options[1])
-            "h" -> takeCardToHand(game, options[1])
-            "b" -> takeCardFromKingdomToHand(game, options[1])
-            "q" -> game.escapeGame(game)
-            else -> throw IllegalArgumentException("Actions: t,u,q,i,d,p,h space and number eg. t 2. For exit q 1")
+            else -> throw IllegalArgumentException("Actions: t,u,i,d space and number eg. t 2.")
         }
     }
+
+    private fun chooseThreeParamsMove(game: Game, options: List<String>) : Game {
+        return when (options[0]) {
+            "p" -> playCardFromHand(game, options[1], options[2])
+            "h" -> takeCardToHand(game, options[1], options[2])
+            else -> throw IllegalArgumentException("Actions: p,h space and area [k,b]and number eg. t 2. For exit q 1")
+        }
+    }
+
 
     private fun makeUntapMove(game: Game, cardNumber: String): Game {
         game.untapTableCard(cardNumber.toInt())
@@ -74,23 +95,32 @@ class GameRunner {
         return game
     }
 
-    private fun playCardFromHand(game: Game, cardNumber: String): Game {
-        game.checkPutCardOnTable(cardNumber.toInt())
-        return game
+    private fun playCardFromHand(game: Game, areaName: String, cardNumber: String): Game {
+        when (areaName) {
+            "b" -> {
+                    game.checkPutCardOnTable(cardNumber.toInt())
+                    return game
+                   }
+            "k" -> {
+                    game.checkPutCardOnTableKingdom(cardNumber.toInt())
+                    return game
+                   }
+            else -> throw IllegalArgumentException("Please use proper area to play card")
+        }
+
     }
 
-    private fun playCardFromHandToKingdom(game: Game, cardNumber: String): Game {
-        game.checkPutCardOnTableKingdom(cardNumber.toInt())
-        return game
-    }
-
-    private fun takeCardToHand(game: Game, cardNumber: String): Game {
-        game.checkTakeCardToHand(cardNumber.toInt())
-        return game
-    }
-
-    private fun takeCardFromKingdomToHand(game: Game, cardNumber: String): Game {
-        game.checkTakeCardFromKingdomToHand(cardNumber.toInt())
-        return game
+    private fun takeCardToHand(game: Game, areaName: String, cardNumber: String): Game {
+        when (areaName) {
+            "b" -> {
+                game.checkTakeCardToHand(cardNumber.toInt())
+                return game
+            }
+            "k" -> {
+                game.checkTakeCardFromKingdomToHand(cardNumber.toInt())
+                return game
+            }
+            else -> throw IllegalArgumentException("Please use proper area to play card")
+        }
     }
 }
