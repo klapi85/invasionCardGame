c3abb587957c5cc8ff6ed4f8bfd3925479e4cbcf
Wojciech Kuchta <klapi85@gmail.com>
2018-06-28 23:06:43 +0200

    shuffle deck at start

--- src/game/domain/Deck.kt
+++ src/game/domain/Deck.kt
@@ -4,17 +4,21 @@ class Deck(
     ) {
     var cards: MutableList<Card> =
         mutableListOf(
-            Card("Pink Goblin", 1, 1, 1),
-            Card("Dragon",5, 4, 5),
+            Card("Goblin", 1, 1, 1),
+            Card("Goblin", 1, 1, 1),
             Card("Goblin", 1, 1, 1),
             Card("Minotaur",2, 2, 2),
             Card("Hydra", 3, 4, 3),
-            Card("Red Dragon",4, 5, 5),
-            Card("Dragon",5, 4, 5),
-            Card("Dragon",5, 4, 5),
+            Card("Green Dragon",4, 4, 4),
+            Card("Red Dragon",5, 4, 5),
+            Card("Red Dragon",5, 4, 5),
             Card("Black Dragon",6, 4, 6)
-
         )
+
+    init {
+        this.cards.shuffle()
+        // shuffle requires Kotlin v.1.2
+    }
 
     fun removeFromDeck(number: Int): Card =
         this.cards.removeAt(number)
